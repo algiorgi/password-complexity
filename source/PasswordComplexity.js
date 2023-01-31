511cf12c8aad815758a24c440aebcf798a4e4889
Alejandro Giorgi <agiorgi@inrule.com>
2023-01-31 17:56:24 -0300

    Hace pasar todas las pruebas pero faltan casos

--- source/PasswordComplexity.js
+++ source/PasswordComplexity.js
@@ -7,9 +7,22 @@ const PasswordComplexityValues = {
 const calculateComplexity = (password) => {
 
     const passwordLength = password.length;
+    const numbers = countNumbers(password);
 
-    let complexity = passwordLength >= 8 ? PasswordComplexityValues.GOOD : PasswordComplexityValues.WEAK;
+    let complexity = passwordLength >= 8 && numbers >= 2 ? PasswordComplexityValues.EXCELLENT : 
+    (passwordLength >= 8 || numbers >= 2) ? PasswordComplexityValues.GOOD : PasswordComplexityValues.WEAK
     return complexity;
+}
+
+function countNumbers(password) {
+    let count = 0;
+    for (i = 0; i < password.length; i++) {
+        if (isNaN(password[i]) == false) {
+            count++;
+        }
+    }
+
+    return count;
 }
 
 module.exports = { calculateComplexity, PasswordComplexityValues }
